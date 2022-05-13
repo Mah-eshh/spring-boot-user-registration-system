5c2e574518193643824b6743d66b2d9c653c0cb4
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 18:02:45 +0530

    Create both User & Role model classes

--- src/main/java/me/mahezh/registration/model/User.java
+++ src/main/java/me/mahezh/registration/model/User.java
@@ -1,5 +1,83 @@
 package me.mahezh.registration.model;
 
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames="email"))
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name ="first_name")
+	private String firstName;
+	
+	@Column(name ="last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	private Collection<Role> roles;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId (Long id) {
+		this.id= id;
+	} 
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName () {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}	
+	public String getEmail() {
+		return email;
+	}	
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	} 
+	public Collection<Role>getRoles(){
+		return roles;
+	} 
+	public void setRoles(Collection<Role> roles){
+		this.roles=roles;
+	}
+	
+	public User(String fistName, String lastName, String email, String password) {
+		super();
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.email=email;
+		this.password=password;
+		this.roles=roles;		
+		
+	}
+	
 }
