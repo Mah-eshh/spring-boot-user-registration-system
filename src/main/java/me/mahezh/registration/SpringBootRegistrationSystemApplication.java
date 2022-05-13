65e149f0432002a1c5b2f682e2c570dd4ec8b115
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 13:31:04 +0530

    Initial commit

---
+++ src/main/java/me/mahezh/registration/SpringBootRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package me.mahezh.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mahezh/registration/SpringBootRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mahezh.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
