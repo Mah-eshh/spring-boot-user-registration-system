5b76dd081a434621e11874ee905b73ccda70819c
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 23:10:23 +0530

    Create frontend of user registration

--- src/main/java/me/mahezh/registration/SpringBootRegistrationSystemApplication.java
+++ src/main/java/me/mahezh/registration/SpringBootRegistrationSystemApplication.java
@@ -2,8 +2,9 @@ package me.mahezh.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringBootRegistrationSystemApplication {
 
 	public static void main(String[] args) {
