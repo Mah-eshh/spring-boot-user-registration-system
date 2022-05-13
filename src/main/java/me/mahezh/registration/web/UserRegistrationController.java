35ac0a8c930725b49ce692973244134da7916279
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 19:49:04 +0530

    Inject userService

--- src/main/java/me/mahezh/registration/web/UserRegistrationController.java
+++ src/main/java/me/mahezh/registration/web/UserRegistrationController.java
@@ -1,5 +1,20 @@
 package me.mahezh.registration.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.mahezh.registration.service.UserService;
+
+@Controller
+@RequestMapping("/registration")
+
 public class UserRegistrationController {
 
+	private UserService userService;
+
+	public UserRegistrationController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
 }
