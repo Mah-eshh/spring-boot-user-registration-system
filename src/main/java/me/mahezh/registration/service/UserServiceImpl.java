a415016f6880e9b12da7daf27d7138914545560c
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 19:40:22 +0530

    Create User save method in UserService class

--- src/main/java/me/mahezh/registration/service/UserServiceImpl.java
+++ src/main/java/me/mahezh/registration/service/UserServiceImpl.java
@@ -1,5 +1,35 @@
 package me.mahezh.registration.service;
 
-public class UserServiceImpl {
+import java.util.Arrays;
+
+import javax.management.relation.Role;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import me.mahezh.registration.model.User;
+import me.mahezh.registration.repository.UserRepository;
+import me.mahezh.registration.web.dto.UserRegistrationDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public User save(UserRegistrationDto registrationDto) {
+
+		User user = new User(registrationDto.getFirstName(),
+				registrationDto.getLastName(),
+				registrationDto.getEmail(),
+				registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+		return userRepository.save(user);
+	}
 
 }
