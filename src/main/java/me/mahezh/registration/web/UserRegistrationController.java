ee308dc1cbb7b9b1233d4b4a0e7586eea9107742
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 19:54:52 +0530

    Create method handler

--- src/main/java/me/mahezh/registration/web/UserRegistrationController.java
+++ src/main/java/me/mahezh/registration/web/UserRegistrationController.java
@@ -1,15 +1,18 @@
 package me.mahezh.registration.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.mahezh.registration.service.UserService;
+import me.mahezh.registration.web.dto.UserRegistrationDto;
 
 @Controller
 @RequestMapping("/registration")
 
 public class UserRegistrationController {
-
+	
+	//Inject userService
 	private UserService userService;
 
 	public UserRegistrationController(UserService userService) {
@@ -17,4 +20,12 @@ public class UserRegistrationController {
 		this.userService = userService;
 	}
 	
+	//method handler
+	
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect/registration?success";
+	}
+	
+
 }
