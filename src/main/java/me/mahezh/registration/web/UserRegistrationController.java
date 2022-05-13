63e1ed8ad00c854d6e6354cdd8845a40b05c0931
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-13 20:27:45 +0530

    Add getmapping anotation to get all the details

--- src/main/java/me/mahezh/registration/web/UserRegistrationController.java
+++ src/main/java/me/mahezh/registration/web/UserRegistrationController.java
@@ -1,7 +1,9 @@
 package me.mahezh.registration.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.mahezh.registration.service.UserService;
@@ -20,8 +22,14 @@ public class UserRegistrationController {
 		this.userService = userService;
 	}
 	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
 	//method handler
 	
+	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
 		return "redirect/registration?success";
