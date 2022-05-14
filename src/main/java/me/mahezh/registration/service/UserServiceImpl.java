bf35e2070946545c66e29d8895e63094ce17236a
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-14 13:00:42 +0530

    Implement backend for login feature

--- src/main/java/me/mahezh/registration/service/UserServiceImpl.java
+++ src/main/java/me/mahezh/registration/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package me.mahezh.registration.service;
 
 import java.util.Arrays;
-
-
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -38,9 +40,22 @@ public class UserServiceImpl implements UserService {
 
 
 	@Override
+	
+	
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		//retrieve user object from the repository
+		User user = userRepository.findByEmail(username);
+		//check whter null or not
+		if(user == null) {
+			throw new UsernameNotFoundException("Invalid username or password.");
+		}
+		//if user is not null create a user object, and pass email password, roles to the user object
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));		
 	}
-
+	//map
+	
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+		//convert roles into stream.here stream is a map method. 
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+	}
 }
