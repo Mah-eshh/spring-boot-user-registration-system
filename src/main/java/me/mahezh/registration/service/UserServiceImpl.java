982f4cd529e9fd36b948b1c9f761e7bc76e8a868
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-14 13:06:07 +0530

    Implement backend for login feature

--- src/main/java/me/mahezh/registration/service/UserServiceImpl.java
+++ src/main/java/me/mahezh/registration/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import me.mahezh.registration.model.Role;
@@ -19,8 +20,11 @@ import me.mahezh.registration.web.dto.UserRegistrationDto;
 @Service
 public class UserServiceImpl implements UserService {
 
-	@Autowired
+	
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public UserServiceImpl(UserRepository userRepository) {
 		super();
@@ -30,11 +34,10 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User save(UserRegistrationDto registrationDto) {
-
-		User user = new User(registrationDto.getFirstName(),
-				registrationDto.getLastName(),
-				registrationDto.getEmail(),
-				registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+		User user = new User(registrationDto.getFirstName(), 
+				registrationDto.getLastName(), registrationDto.getEmail(),
+				passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+		
 		return userRepository.save(user);
 	}
 
